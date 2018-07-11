56ad0c4f1e0a727cf06ee04a40577ea31e6fc24e
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:28:02 +0000

    automatically backed up by learn

--- this.rb
+++ this.rb
@@ -2,23 +2,23 @@ def least_coins
     cents = 100
   coin = {quarters => 0, dimes => 0, nickels => 0, pennies => 0}
   
-  
-  while cents >= 25
+  quarters = cents/25
+  if cents >= 25
     quarters +1
     cents -= 25
-  end
-    
-    while 10<=cents<25
+ 
+ dimes = cents/10
+    if 10<=cents<25
     dimes + 1
     cents -= 10
   end
-    
-    while 5<=cents<10
+nickels = cents/5
+   if 5<=cents<10
     nickels + 1
     cents -= 5
   end
-    
-     while 1<=cents<5
+ pennies = cents/1
+    if 1<=cents<5
     pennies + 1
     cents -=1
   end
