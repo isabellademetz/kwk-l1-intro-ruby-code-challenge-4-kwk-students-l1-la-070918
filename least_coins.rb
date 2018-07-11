a502c097985bebe356e27b8f2c8351e59089d6b3
isabellademetz <isabella21136@gmail.com>
2018-07-11 22:58:45 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,12 +1,10 @@
 #write out your code here
 
 def least_coins(cents)
-  amount = 1
-"quarters" = 0.25
-"dimes" = 0.10
-"nickels" = 0.05
-"pennies" = 0.01
-while amount > 0.25
-put "1 quarter"
-quarter -= 0.25
-end
+  amount = 
+  coin = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+  while coin > 25
+  :quarter +=1
+  amount -= 25
+  
+  
