8944cc64b2e4cbce36d52d584397470ef64cad2d
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:01:39 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
 #write out your code here
 
 def least_coins(cents)
-  amount = 
+  amount = 100
   coin = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
   while amount > 25
   :quarter +=1
