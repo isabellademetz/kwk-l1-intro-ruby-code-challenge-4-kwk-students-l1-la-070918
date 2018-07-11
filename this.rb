26c1806e911ead8ede12f77a15feb5905e80f8b2
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:41:43 +0000

    automatically backed up by learn

--- this.rb
+++ this.rb
@@ -2,7 +2,7 @@ def least_coins(cents)
   coin = {quarters => 0, dimes => 0, nickels => 0, pennies => 0}
   
   quarters = cents/25
-  if quarters = 0
+  if quarters == 0
     quarters += 0
    elsif quarters > 1
     quarters += 1
@@ -10,14 +10,14 @@ def least_coins(cents)
   end
 
  dimes = cents/10
-    if dimes = 0
+    if dimes == 0
     dimes += 0
     elsif dimes > 1
     dimes += 1
     cents -= 10
 end
 nickels = cents/5
-if nickels = 0
+if nickels == 0
     nickels += 0
    elsif nickels > 1
     nickels += 1
@@ -25,7 +25,7 @@ if nickels = 0
  end 
  
  pennies = cents/1
- if pennies = 0
+ if pennies == 0
     pennies += 0
     else pennies > 1
     pennies += 1
