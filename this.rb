f389fb115b247c0079c49451ce5ee5915c1ecdba
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:13:04 +0000

    automatically backed up by learn

--- this.rb
+++ this.rb
@@ -1,2 +1,8 @@
 def least_coins
-  
+    cents = 100
+  coin = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+  quarters = cents/25
+  if cents > 25
+    :quarters += 1
+    elsif :quarters += 0
+    
