d2e73d18208c1a7587ffc17d5d0059a3a7cc433f
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:28:51 +0000

    automatically backed up by learn

--- this.rb
+++ this.rb
@@ -4,22 +4,22 @@ def least_coins
   
   quarters = cents/25
   if cents >= 25
-    quarters +1
+    quarters += 1
     cents -= 25
  end
  dimes = cents/10
     if 10<=cents<25
-    dimes + 1
+    dimes += 1
     cents -= 10
   end
 nickels = cents/5
    if 5<=cents<10
-    nickels + 1
+    nickels += 1
     cents -= 5
   end
  pennies = cents/1
     if 1<=cents<5
-    pennies + 1
+    pennies += 1
     cents -=1
   end
     puts coin
