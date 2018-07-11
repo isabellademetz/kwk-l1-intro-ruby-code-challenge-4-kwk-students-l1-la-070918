3bc6477b9afa3b5f3f8fc3b374346291b3b3c1ea
isabellademetz <isabella21136@gmail.com>
2018-07-11 23:06:12 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -6,21 +6,22 @@ def least_coins(cents)
   while amount > 25
   :quarters +=1
   amount -= 25
-end
+
   
     while amount > 10
   :dimes +=1
   amount -= 10
-end
+
   
     while amount > 0.5
   :nickels +=1
   amount -= 0.5
-end
+
   
   while amount > 0.1
   :pennies +=1
   amount -= 0.1
-end 
+
   puts coin
+end
   
